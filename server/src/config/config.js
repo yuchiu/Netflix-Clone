6e9e12fcf34a6bd5854cf3b81fe5551dc6d9a2ec
yu chiu <yuchiu2002@hotmail.com>
2018-07-16 14:07:30 -0400

    restructured dir

--- server/src/config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 8000
-}
--- server/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    app.get('/',
-            function (req, res) {
-                res.render("index")
-            }
-        )
-}
