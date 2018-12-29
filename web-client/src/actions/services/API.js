fbc87556da660606a7910166b85904212be6bb6f
yuchiu <yuchiu2002@hotmail.com>
2018-12-28 19:58:08 -0500

    fixed jwt malform error

--- web-client/src/actions/services/API.js
+++ web-client/src/actions/services/API.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 import localStore from "@/utils/localStore";
 
-const apiV1 = () =>
-  axios.create({
-    baseURL: `http://localhost:3030/api/v1`,
-    headers: {
-      Authorization: `Bearer ${localStore.getToken()}`
-    }
+const apiV1 = () => {
+  // authenticate user if jwt token is in localstore
+  if (localStore.getToken()) {
+    return axios.create({
+      baseURL: `http://localhost:3030/api/v1`,
+      headers: {
+        Authorization: `Bearer ${localStore.getToken()}`
+      }
+    });
+  }
+
+  return axios.create({
+    baseURL: `http://localhost:3030/api/v1`
   });
+};
 
 export { apiV1 };
--- web-server/src/middlewares/authenticateJWT.js
+++ web-server/src/middlewares/authenticateJWT.js
@@ -2,13 +2,12 @@ import jwt from "jsonwebtoken";
 
 export default () => (req, res, next) => {
   const authorizationHeaader = req.headers.authorization;
-  let result;
   if (authorizationHeaader) {
     const token = req.headers.authorization.split(" ")[1]; // Bearer <token>
     const options = {};
     try {
       // verify makes sure that the token hasn't expired and has been issued by us
-      result = jwt.verify(token, process.env.JWT_SECRET, options);
+      const result = jwt.verify(token, process.env.JWT_SECRET, options);
 
       // Let's pass back the decoded token to the request object
       req.user = result;
